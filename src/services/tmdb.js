9a1613147d5618fa1211f7f1e9a2f9169c4d0518
Mahmoud Enany <enanymahmoud24@gmail.com>
2026-06-30 03:45:38 +0300

    Add services/tmdb.js

---
+++ src/services/tmdb.js
@@ -0,0 +1,22 @@
+const BASE_URL = 'https://api.themoviedb.org/3'
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY
+
+export const getPopularMovies = () => {
+  return fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`)
+    .then(res => res.json())
+}
+
+export const getTopRatedMovies = () => {
+  return fetch(`${BASE_URL}/movie/top_rated?api_key=${API_KEY}`)
+    .then(res => res.json())
+}
+
+export const getUpcomingMovies = () => {
+  return fetch(`${BASE_URL}/movie/upcoming?api_key=${API_KEY}`)
+    .then(res => res.json())
+}
+
+export const getNowPlayingMovies = () => {
+  return fetch(`${BASE_URL}/movie/now_playing?api_key=${API_KEY}`)
+    .then(res => res.json())
+}
