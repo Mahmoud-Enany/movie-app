627c278e9af4666205320d38a310c7d1e6eaac52
Mahmoud Enany <enanymahmoud24@gmail.com>
2026-07-01 04:54:22 +0300

    Add Router, MainLayout, and global styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-function App() 
-{
-  return <div>Movie App</div>
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import MainLayout from './layouts/MainLayout'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+import NotFound from './pages/NotFound'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainLayout />,
+    children: [
+      { index: true, element: <Home /> },
+      { path: 'movie/:id', element: <MovieDetails /> },
+    ]
+  },
+  { path: '*', element: <NotFound /> }
+])
+
+function App() {
+  return <RouterProvider router={router} />
 }
 
 export default App
